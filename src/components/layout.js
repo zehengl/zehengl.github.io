33708f00ae2a662fd7a22b513b2407c88f8c2153
Zeheng Li <imzehengl@gmail.com>
2021-06-14 00:37:22 -0600

    fix burger menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,10 @@ import { version } from "../../package.json"
 
 function Layout(props) {
   const { title, children } = props
+  const [isOpen, setisOpen] = React.useState(false)
+  function handleClick() {
+    setisOpen(!isOpen)
+  }
 
   return (
     <div className="overflow-x-hidden bg-gray-100">
@@ -12,33 +16,44 @@ function Layout(props) {
             <div>
               <p className="text-xl font-bold text-gray-800 md:text-2xl">{title}</p>
             </div>
-            <div>
-              <button
-                type="button"
-                className="block text-gray-800 hover:text-gray-600 focus:text-gray-600 focus:outline-none md:hidden"
+            <button type="button" className="block md:hidden" onClick={handleClick}>
+              <svg
+                className="h-6 w-6 fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
               >
-                <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-                  <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
-                </svg>
-              </button>
-            </div>
+                {isOpen && (
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+                  />
+                )}
+                {!isOpen && (
+                  <path
+                    fillRule="evenodd"
+                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                  />
+                )}
+              </svg>
+            </button>
           </div>
-          <div className="flex-col hidden md:flex md:flex-row md:-mx-4">
+          <div className={`md:flex ${isOpen ? "block" : "hidden"} `}>
             <a
               href="/"
-              className="my-1 text-gray-800 hover:text-blue-500 md:mx-4 md:my-0"
+              className="flex my-1 text-gray-800 hover:text-blue-500 md:mx-4 md:my-0"
             >
               Home
             </a>
             <a
               href="/tags"
-              className="my-1 text-gray-800 hover:text-blue-500 md:mx-4 md:my-0"
+              className="flex my-1 text-gray-800 hover:text-blue-500 md:mx-4 md:my-0"
             >
               Tags
             </a>
             <a
               href="/uses"
-              className="my-1 text-gray-800 hover:text-blue-500 md:mx-4 md:my-0"
+              className="flex my-1 text-gray-800 hover:text-blue-500 md:mx-4 md:my-0"
             >
               Uses
             </a>
